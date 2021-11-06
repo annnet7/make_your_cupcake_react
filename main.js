7c9bb9aa90179c899b07f7e006100f73017849f6
annnet7 <51447220+annnet7@users.noreply.github.com>
2021-11-06 18:09:16 +0200

    elements to select the cake

--- main.js
+++ main.js
@@ -9,8 +9,24 @@ document.addEventListener("DOMContentLoaded", () => {
         .to("#buttonToStart", {duration: 1, y: -30, opacity: 0, ease: "pover1.InOut"}, "-=0.9")
         .set("#leftCol", {css:{backgroundImage:'none'}})
         .set(".cakeImage", {css:{display:'block'}})
-        .to("#leftCol", {duration: 0.7, x: '0%', ease: "pover1.InOut"}, "+=0.0");
+        .set(".selectToCart", {css:{display:'block'}})
+        .set(["#header","#buttonToStart"], {css:{display:'none'}})
+        .to("#leftCol", {duration: 0.7, x: '0%', ease: "pover1.InOut"}, "+=0.0")
+        .from(".selectToCart", {duration: 1, y: -30, opacity: 0, ease: "pover1.InOut"}, "-=0.7");
 
     })
+
+    //set the cake part
+    //I will make auto generation of data base tables
+    let cakeArray = [];
+    for(let i=0;i<3;i++){
+        cakeArray.push({id: i, nameText:'cake'+(i+1), imgUrl: 'assets/cakes/cake'+(i+1)+'.jpg', description:'description of cake num'+i})
+    }
+    let topingArray = [];
+    for(let i=0;i<11;i++){
+        topingArray.push({id: i, nameText:'toping'+(i+1), imgUrl: 'assets/cakes/top'+(i+1)+'.jpg',  description:'description of toping num'+i})
+    }
+    console.log(cakeArray);
+    console.log(topingArray);
 
 });
